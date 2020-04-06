08a5b35c70919cec95b4e756e4f739283a37fce6
FlorentFERRAND <florent.ferrand.cdiscount@gmail.com>
2020-04-06 12:31:28 +0200

    Delete Model _color from Methods

--- Models/Methods.cs
+++ Models/Methods.cs
@@ -7,8 +7,6 @@
         public string NameMethod { get; set; }
         public string StateMethod { get; set; }
 
-        public string _color = "st-green";
-
         public Methods()
         {
         }

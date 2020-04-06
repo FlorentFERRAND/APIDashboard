d0efb11331a6a6db464ab16467d23e67c8468bd6
FlorentFERRAND <florent.ferrand.cdiscount@gmail.com>
2020-04-06 11:26:49 +0200

    Rename Models and edit attributes name

---
+++ Models/CalendarM1.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDashboard.Models
+{
+    public class CalendarM1
+    {
+        public string TitleCalendarM1 { get; set; }
+        public string MonthCalM1 {get;set;}
+        public string DayOfTheWeekDayStatusOfTheDay1M1 { get; set; }
+        public string DayOfTheWeekDayStatusOfTheDay2M1 { get; set; }
+        public string DayOfTheWeekDayStatusOfTheDay3M1 { get; set; }
+        public string DayOfTheWeekDayStatusOfTheDay4M1 { get; set; }
+        public string DayOfTheWeekDayStatusOfTheDay5M1 { get; set; }
+
+        public CalendarM1()
+        {
+        }
+    }
+}

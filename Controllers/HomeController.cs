071b95e3afaa93c6ae7f8242dfc56e3ca2d6169b
FlorentFERRAND <florent.ferrand.cdiscount@gmail.com>
2020-05-11 16:13:20 +0200

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using APIDashboard.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace APIDashboard.Controllers
 {

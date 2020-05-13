254a31787dc00538b89099cec79a7742881f2e11
FlorentFERRAND <florent.ferrand.cdiscount@gmail.com>
2020-05-13 17:53:37 +0200

    Add new Controller for status by method for PreProduction page

--- Controllers/PreProductionController.cs
+++ Controllers/PreProductionController.cs
@@ -14,7 +14,7 @@ namespace APIDashboard.Controllers
         public IActionResult PreProduction(string titleInfos, string infos, string stateInfos,
         string titlePC, string statusbyMetPC, int numberPC,
         string titleIss, int numIss, string expliIss, string stIss, string dateResoIss,
-        string titleMet, string cateMet, string nameMet, string staMet,
+        string titleMet, string cateMet, string nameMet, string staMet, string tooltipStaMet,
         string calsCTitle, string monthCalM1, string day1M1, string day2M1, string day3M1, string day4M1, string day5M1,
         string monthCalM2, string day1M2, string day2M2, string day3M2, string day4M2, string day5M2)
         {            
@@ -35,6 +35,7 @@ namespace APIDashboard.Controllers
                 CategoryMethod = cateMet,
                 NameMethod = nameMet,
                 StateMethod = staMet,
+                TooltipStateMethod = tooltipStaMet,
                 CalendarsCardTitle = calsCTitle,
                 MonthCalM1 = monthCalM1,
                 DayOfTheWeekDayStatusOfTheDay1M1 = day1M1,

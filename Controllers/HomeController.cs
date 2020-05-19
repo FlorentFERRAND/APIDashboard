d4a14128266e094f23175f3b31dcc0fe77126838
FlorentFERRAND <florent.ferrand.cdiscount@gmail.com>
2020-05-19 09:13:11 +0200

    Remove unnecessary usings

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using APIDashboard.Models;
-using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
 
 namespace APIDashboard.Controllers
 {

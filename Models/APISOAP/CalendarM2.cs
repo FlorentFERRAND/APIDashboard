628bcb7083325c139101c0e1ffc8e3f906a3ae69
FlorentFERRAND <florent.ferrand.cdiscount@gmail.com>
2022-02-08 15:04:37 +0100

    Edit Models for API SOAP pages

---
+++ Models/APISOAP/CalendarM2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDashboard.Models
+{
+    public class CalendarM2
+    {
+        public string MonthCalM2 {get;set;}
+        public string DayOfTheWeekDayStatusOfTheDay1M2 { get; set; }
+        public string DayOfTheWeekDayStatusOfTheDay2M2 { get; set; }
+        public string DayOfTheWeekDayStatusOfTheDay3M2 { get; set; }
+        public string DayOfTheWeekDayStatusOfTheDay4M2 { get; set; }
+        public string DayOfTheWeekDayStatusOfTheDay5M2 { get; set; }
+
+        public CalendarM2()
+        {
+        }
+    }
+}
---
+++ Models/APISOAP/CalendarsTitle.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDashboard.Models
+{
+    public class CalendarsTitle
+    {
+        public string CalendarsCardTitle { get; set; }
+
+        public CalendarsTitle()
+        {
+        }
+    }
+}

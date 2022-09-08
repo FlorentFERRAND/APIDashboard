0b9106163fcca88a188f1c824d8ca558c54c1415
FlorentFERRAND <florent.ferrand.cdiscount@gmail.com>
2022-09-08 16:54:03 +0200

    Add new Model for API V2

---
+++ Models/API/MethodsV2.cs
@@ -0,0 +1,17 @@
+namespace APIDashboard.Models
+{
+    public class MethodsV2
+    {
+        public string TitleMethodV2 { get; set; }
+        public string VersionMethodV2 { get; set; }
+        public string CategoryMethodV2 { get; set; }
+        public string TypeMethodV2 { get; set; }
+        public string NameMethodV2 { get; set; }
+        public string StateMethodV2 { get; set; }
+        public string TooltipStateMethodV2 { get; set; }
+
+        public MethodsV2()
+        {
+        }
+    }
+}
